aebdf4521ac283d69f4b2ce8b9e1c29948b5d8e3
galibcse19 <mrgalib40@gmail.com>
2024-09-01 02:19:31 +0600

    payment system

--- bistro-boss-client/src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ bistro-boss-client/src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,11 +1,27 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useCart from "../../../hooks/useCart";
 
  
 
 const CheckoutForm = () => {
 
+    const [error,setError] = useState('');
+    const [clientSecret,setClientSecret] = useState('');
     const stripe = useStripe();
     const elements = useElements();
+    const axiosSecure = useAxiosSecure();
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total,item)=> total+item.price,0)
+
+    useEffect(() =>{
+        axiosSecure.post('/create-payment-intent',{price: totalPrice})
+        .then(res =>{
+            console.log(res.data.clientSecret);
+            setClientSecret(res.data.clientSecret);
+        })
+    },[axiosSecure,totalPrice])
 
     const handleSubmit = async(event)=>{
         event.preventDefault();
@@ -25,9 +41,11 @@ const CheckoutForm = () => {
         })
         if(error){
             console.log('payment error',error);
+            setError(error.message);
         }
         else{
             console.log('payment method',paymentMethod);
+            setError('');
         }
     }
     return (
@@ -48,9 +66,10 @@ const CheckoutForm = () => {
                 },
                 }}
             />
-            <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe}>
+            <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe || !clientSecret}>
                 Pay
             </button>
+            <p className="text-red-700">{error}</p>
         </form>
     );
 };
